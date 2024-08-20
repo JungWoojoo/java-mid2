176df327ac8769604f4bc767a911e0ccbd87b25d
Woojoo JUNG <jwj5045@gmail.com>
2024-08-20 22:37:31 +0900

    제 8강 컬렉션 프레임워크 - set 문제풀이

--- src/collection/set/javaset/JavaSetMain.java
+++ src/collection/set/javaset/JavaSetMain.java
@@ -10,16 +10,17 @@ public class JavaSetMain {
         run(new TreeSet<>());
     }
 
-    private static void run(Set<String> set) {
+    private static void run(Set<String> set){
         System.out.println("set = " + set.getClass());
         set.add("C");
         set.add("B");
         set.add("A");
         set.add("1");
         set.add("2");
+        System.out.println("set = " + set);
 
         Iterator<String> iterator = set.iterator();
-        while (iterator.hasNext()) {
+        while (iterator.hasNext()){
             System.out.print(iterator.next() + " ");
         }
         System.out.println();
